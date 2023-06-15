57c220b6cfb214474166684dc87ff8c40416a2e1
Timothy Wang <2556936w@student.gla.ac.uk>
2023-06-15 03:37:36 +0100

    update intro

--- intro.py
+++ intro.py
@@ -12,6 +12,8 @@ members = ""
 # Display messages in the console
 print("\nWe are " + team_name + "!\n\nWe're here to build rockets!\n")
 
+print("Below are our team members:")
+print(members)
 
 # Play around with variables and printing below!
 print("")
